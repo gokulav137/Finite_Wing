974ac270f6a597aa35068f62c1d05a303f21c344
gokulav137 <52277763+gokulav137@users.noreply.github.com>
2020-07-04 23:10:39 +0530

    Update Finite_Wing.py
    plot_gamma function  updated with a line style option

--- Finite_Wing.py
+++ Finite_Wing.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg
+from itertools import cycle
 
 
 class FiniteWing:
@@ -133,11 +134,16 @@ class SimpleTapered:
         return self.a0, self.alpha0, self.c
 
 
-def plot_gamma(y_data, x_data, wing_type, legends):
+def plot_gamma(y_data, x_data, wing_type, legends, line_type=False):
+    if line_type:
+        lines = ["-", "--"]
+    else:
+        lines = [None]
+    linecycler = cycle(lines)
     plt.ylabel("Bound circulation ()")
     plt.xlabel("Span wise axis (m)")
     for data in y_data:
-        plt.plot(x_data, data)
+        plt.plot(x_data, data, linestyle=next(linecycler))
     plt.legend(legends)
     plt.title("Bound Circulation for "+wing_type+" Chord distribution")
     plt.show()
@@ -158,14 +164,14 @@ def plot_a(dataset, wing_type, legends=None):
 
 
 W_elliptic = FiniteWing(SimpleElliptical(a0_val=2*np.pi, alpha0_val=0))
-W_elliptic.solve(alpha=5*np.pi/180, no_seg=1000, no_freq=40)
+W_elliptic.solve(alpha=5*np.pi/180, no_seg=40, no_freq=40)
 
 num_gamma = W_elliptic.simulate(v_inf=10, sharpness=40)
 ana_gamma = W_elliptic.simulate_analytical(v_inf=10, sharpness=40, alpha=5*np.pi/180)
 elliptical_a = W_elliptic.get_a()[:10]
 cord_pts = W_elliptic.calc_cord_pts(n=40)
 
-plot_gamma([num_gamma, ana_gamma], cord_pts, "Elliptical", ["Numerical Value", "Analytical Value"])
+plot_gamma([num_gamma, ana_gamma], cord_pts, "Elliptical", ["Numerical Value", "Analytical Value"], line_type=True)
 plot_a([elliptical_a], "Elliptical")
 
 W_tapered = []
@@ -174,6 +180,18 @@ data_a = []
 
 for i, tr in enumerate([1, 2, 3]):
     W_tapered.append(FiniteWing(SimpleTapered(t_ratio=tr, a0_val=2 * np.pi, alpha0_val=0)))
+    W_tapered[i].solve(alpha=5*np.pi/180, no_seg=40, no_freq=40)
+    data_gamma.append(W_tapered[i].simulate(v_inf=10, sharpness=40))
+    data_a.append(W_tapered[i].get_a()[:10])
+cord_pts = W_tapered[0].calc_cord_pts(n=40)
+
+plot_gamma(data_gamma, cord_pts, "Tapered", ["Taper ratio: 1", "Taper ratio: 2", "Taper ratio: 3"])
+plot_a(data_a, "Tapered", ["Taper ratio: 1", "Taper ratio: 2", "Taper ratio: 3"])
+
+data_gamma = []
+data_a = []
+
+for i, tr in enumerate([1, 2, 3]):
     W_tapered[i].solve(alpha=5*np.pi/180, no_seg=100, no_freq=40)
     data_gamma.append(W_tapered[i].simulate(v_inf=10, sharpness=40))
     data_a.append(W_tapered[i].get_a()[:10])
